7d3ab1b4cb02e51087ca17697443977858df1818
Chris <chrisrbredernitz@gmail.com>
2024-06-26 13:50:17 -0600

    add a test for ListingDataFetcher that queries graphQl

---
+++ src/test/java/com/graphql/graphqlplayground/datafetchers/ListingDataFetcherTest.java
@@ -0,0 +1,29 @@
+package com.graphql.graphqlplayground.datafetchers;
+
+import com.graphql.graphqlplayground.models.ListingModel;
+import com.netflix.graphql.dgs.DgsQueryExecutor;
+import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(classes = {DgsAutoConfiguration.class, ListingDataFetcher.class})
+class ListingDataFetcherTest {
+
+    @Autowired
+    DgsQueryExecutor dgsQueryExecutor;
+
+    @Test
+    void givenGetListing_whenQueried_ThenReturnStubTitle() {
+        List<String> listingModels = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { featuredListings { title }}",
+                "data.featuredListings[*].title");
+
+        assertThat(listingModels).contains("Beach house on the edge of the Laertes meteor");
+    }
+}
