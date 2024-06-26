a6f6245fc11a6a376e7c5493eaefaa5ea0d643a0
Chris <chrisrbredernitz@gmail.com>
2024-06-26 13:24:31 -0600

    adding initial files

---
+++ src/test/java/com/graphql/graphqlplayground/GraphqlplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.graphqlplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
